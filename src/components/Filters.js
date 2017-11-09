3fd3756eb15c259cd2e03df545ede52359eecfd2
jffernan <jffernan@comcast.net>
2017-11-08 16:27:25 -0800

    Re-do lab

---
+++ src/components/Filters.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+export default class Filters extends Component {
+
+  handleFilterTypeChange = event =>
+    this.props.onChangeType(event.target.value);
+
+  render() {
+
+    return (
+      <div className="ui form">
+        <h3>Animal type</h3>
+        <div className="field">
+          <select
+            name="type"
+            id="type"
+            value={this.props.filters.type}
+            onChange={this.handleFilterTypeChange}
+          >
+            <option value="all">All</option>
+            <option value="cat">Cats</option>
+            <option value="dog">Dogs</option>
+            <option value="micropig">Micropigs</option>
+          </select>
+        </div>
+
+        <div className="field">
+          <button
+            className="ui secondary button"
+            onClick={this.props.onFindPetsClick}
+          >
+            Find pets
+          </button>
+        </div>
+      </div>
+    );
+  }
+};
---
+++ src/components/PetBrowser.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Pet from './Pet';
+
+export default class PetBrowser extends Component {
+  render() {
+    const renderPets = this.props.pets.map(pet =>
+      <Pet
+        pet={pet}
+        key={pet.id}
+        onAdoptPet={this.props.onAdoptPet}
+        isAdopted={this.props.adoptedPets.includes(pet.id)}
+      />
+    )
+
+    return (
+      <div className="ui cards">
+        {renderPets}
+      </div>
+    );
+  }
+};
